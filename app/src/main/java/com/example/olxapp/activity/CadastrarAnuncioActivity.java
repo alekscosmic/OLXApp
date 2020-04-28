b92a41ef025717c45d8ae39c98e81bd8f26e9ebf
alekscosmic <64270357+alekscosmic@users.noreply.github.com>
2020-04-27 22:18:47 -0300

    Aulas 360 e 361 Utilizando mascáras - telefone e moeda

--- app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
@@ -3,14 +3,47 @@ package com.example.olxapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
+import com.blackcat.currencyedittext.CurrencyEditText;
 import com.example.olxapp.R;
+import com.santalu.maskedittext.MaskEditText;
+
+import java.util.Locale;
 
 public class CadastrarAnuncioActivity extends AppCompatActivity {
+
+    private EditText campoTitulo, campoDescricao;
+    private CurrencyEditText campoValor;
+    private MaskEditText campoTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_anuncio);
+
+        inicializarComponentes();
+
+    }
+
+    public void salvarAnuncio(View view){
+        String valor = campoValor.getText().toString();
+        String telefone= campoTelefone.getText().toString();
+        Log.d("salvar", "salvarAnuncio " +telefone);
+
+    }
+    private void inicializarComponentes(){
+
+        campoTitulo = findViewById(R.id.editTitulo);
+        campoDescricao = findViewById(R.id.editDescricao);
+        campoValor = findViewById(R.id.editValor);
+        campoTelefone = findViewById(R.id.editTelefone);
+
+        //Configura localidade para pt -> portugues BR -> Brasil
+        Locale locale = new Locale("pt","BR");
+        campoValor.setLocale(locale);
+
     }
 }
