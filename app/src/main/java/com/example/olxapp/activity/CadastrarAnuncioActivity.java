83c9812814c20e7e88a2965e821d32c2d4bc2e81
alekscosmic <64270357+alekscosmic@users.noreply.github.com>
2020-04-28 13:18:42 -0300

    Aula 363 Escolhendo fotos do anúncio

--- app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
+++ app/src/main/java/com/example/olxapp/activity/CadastrarAnuncioActivity.java
@@ -1,33 +1,44 @@
 package com.example.olxapp.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.blackcat.currencyedittext.CurrencyEditText;
 import com.example.olxapp.R;
 import com.example.olxapp.helper.Permissoes;
 import com.santalu.maskedittext.MaskEditText;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class CadastrarAnuncioActivity extends AppCompatActivity {
+public class CadastrarAnuncioActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText campoTitulo, campoDescricao;
+    private ImageView imagem1, imagem2, imagem3;
     private CurrencyEditText campoValor;
     private MaskEditText campoTelefone;
 
-    private String [] permissoes = new String[]{
-          Manifest.permission.READ_EXTERNAL_STORAGE
+    private String[] permissoes = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE
     };
+    private List<String> listaFotosRecuperadas = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,21 +51,74 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
 
     }
 
-    public void salvarAnuncio(View view){
+    public void salvarAnuncio(View view) {
         String valor = campoValor.getText().toString();
-        String telefone= campoTelefone.getText().toString();
-        Log.d("salvar", "salvarAnuncio " +telefone);
+        String telefone = campoTelefone.getText().toString();
+        Log.d("salvar", "salvarAnuncio " + telefone);
 
     }
-    private void inicializarComponentes(){
+
+    @Override
+    public void onClick(View v) {
+        // Como isso qualquer item clicado na tela do activity_cadastrar_anuncio saberemos usando parametro v
+        switch (v.getId()) {//Agora eu consigo pegar o ID do item q foi clicado.
+            case R.id.imageCadastro1:
+                escolherImagem(1);
+                break;
+
+            case R.id.imageCadastro2:
+                escolherImagem(2);
+                break;
+
+            case R.id.imageCadastro3:
+                escolherImagem(3);
+                break;
+        }
+
+    }
+
+    public void escolherImagem(int requestCode) {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, requestCode); //Com isso eu consigo saber qual imagem esta sendo chamada
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == Activity.RESULT_OK){
+            //Recuperar imagem
+            Uri imagemSelecionada = data.getData();
+            String caminhoImagem = imagemSelecionada.toString();
+
+           //Configura imagem no ImageView
+           if(requestCode == 1){
+               imagem1.setImageURI(imagemSelecionada);
+           }else if(requestCode == 2){
+               imagem2.setImageURI(imagemSelecionada);
+           }else if(requestCode == 3){
+               imagem3.setImageURI(imagemSelecionada);
+           }
+            listaFotosRecuperadas.add(caminhoImagem);
+
+        }
+    }
+
+    private void inicializarComponentes() {
 
         campoTitulo = findViewById(R.id.editTitulo);
         campoDescricao = findViewById(R.id.editDescricao);
         campoValor = findViewById(R.id.editValor);
         campoTelefone = findViewById(R.id.editTelefone);
+        imagem1 = findViewById(R.id.imageCadastro1);
+        imagem2 = findViewById(R.id.imageCadastro2);
+        imagem3 = findViewById(R.id.imageCadastro3);
+        imagem1.setOnClickListener(this);// A propria classe que vai gerenciar o clique
+        imagem2.setOnClickListener(this);// A propria classe que vai gerenciar o clique
+        imagem3.setOnClickListener(this);// A propria classe que vai gerenciar o clique
 
         //Configura localidade para pt -> portugues BR -> Brasil
-        Locale locale = new Locale("pt","BR");
+        Locale locale = new Locale("pt", "BR");
         campoValor.setLocale(locale);
 
     }
@@ -63,15 +127,15 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         //Verificando se o usuario deu permissao para acessar a galeria de imagens
-        for(int permissaoResultado : grantResults){
-            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+        for (int permissaoResultado : grantResults) {
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
                 alertaValidacaoPermissao();
 
             }
         }
     }
 
-    private void alertaValidacaoPermissao(){
+    private void alertaValidacaoPermissao() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Permissões Negadas");
@@ -79,8 +143,8 @@ public class CadastrarAnuncioActivity extends AppCompatActivity {
         builder.setCancelable(false);
         builder.setPositiveButton("Confirmar ", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-               finish();
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
             }
         });
 
